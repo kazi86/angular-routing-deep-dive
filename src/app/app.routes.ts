4974bc621d3b9c44bb0d9c9f7d4b61dfdf904c6d
Huzaifa <muhammad.huzaifa.tariq@codingkey.com>
2025-01-16 03:01:54 +0500

    routing deep-dive

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import {Routes} from "@angular/router";
+import {TaskComponent} from "./tasks/task/task.component";
+import {NoTaskComponent} from "./tasks/no-task/no-task.component";
+
+export const routes:Routes = [
+  {
+    path:'',
+    component:NoTaskComponent
+  },
+  {
+  path:'tasks',
+  component:TaskComponent
+  }
+]
