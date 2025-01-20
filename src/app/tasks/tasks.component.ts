a12da203a4504c3118e9d51ccbf245c83e69de4d
Huzaifa <muhammad.huzaifa.tariq@codingkey.com>
2025-01-20 09:08:43 +0500

    #284 : Extracting Query Params via Observables

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,9 @@
-import {Component, computed, inject, input} from '@angular/core';
+import {Component, computed, inject, input, OnInit} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import {TasksService} from "./tasks.service";
-import {RouterLink} from "@angular/router";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-tasks',
@@ -12,17 +12,48 @@ import {RouterLink} from "@angular/router";
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit{
 
   public userId = input.required<string>();
 
-  public order = input<string>();
+  // public order = input<'asc'|'desc'>();
 
-  public taskSvc = inject(TasksService);
+  public order = 'asc';
 
   public userTasks = computed(()=>{
-   return this.taskSvc.allTasks().filter((task)=>{
+    return this.taskSvc.allTasks().filter((task)=>{
       return task.userId === this.userId();
     })
   });
+
+  public taskSvc = inject(TasksService);
+
+  private activatedRoute = inject(ActivatedRoute);
+
+  public ngOnInit(){
+
+    this.activatedRoute.queryParams.subscribe({
+      next:(data)=>{
+
+        this.userTasks().sort((a,b)=>{
+
+            if(data['order'] === 'asc'){
+
+              this.order = 'asc';
+
+              return a.id > b.id ? 1 : -1;
+
+            }else{
+
+              this.order = 'desc';
+
+              return a.id > b.id ? -1 : 1;
+
+            }
+
+          });
+      }
+    });
+  }
+
 }
